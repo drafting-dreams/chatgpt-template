752dab9f0e8a7e31712dbbe0ab03f93037ced926
liuxiaoyang <xiaoyang.liu@shopee.com>
2023-10-16 19:10:37 +0800

    feat: use auth token for higher limit rates

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,12 +1,16 @@
-import { ChakraProvider, Button, Input, Text } from '@chakra-ui/react'
+import { ChakraProvider, Button, Link, Input, Text } from '@chakra-ui/react'
 import { useState, useEffect } from 'react'
 import browser from 'webextension-polyfill'
 
 function App() {
   const [repoInfo, setRepoInfo] = useState('')
+  const [token, setToken] = useState('')
   useEffect(() => {
-    browser.storage.local.get().then(({ owner, repo, path }) => {
+    browser.storage.local.get().then(({ owner, repo, path, token }) => {
       if (owner && repo && path) setRepoInfo([owner, repo, path].join('/'))
+      if (token) {
+        setToken(token)
+      }
     })
   }, [])
 
@@ -16,6 +20,17 @@ function App() {
         <h1>Chatgpt-template</h1>
         <Text>{'Github information: ({owner}/{repo}/{path})'}</Text>
         <Input onChange={(e) => setRepoInfo(e.currentTarget.value)} value={repoInfo} />
+        <Text>
+          Personal Access Token{' '}
+          <Link href="https://github.com/settings/tokens/new" isExternal>
+            create
+          </Link>
+        </Text>
+        <Input
+          onChange={(e) => setToken(e.currentTarget.value)}
+          value={token ? '*'.repeat(token.length) : ''}
+          type="password"
+        />
         <Button
           onClick={() => {
             const [owner, repo, path] = repoInfo.split('/')
@@ -23,6 +38,7 @@ function App() {
               owner,
               repo,
               path,
+              token,
             })
           }}
         >
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -4,18 +4,20 @@ import { Octokit } from 'octokit'
 
 function App() {
   const updateTemplates = () => {
-    browser.storage.local.get().then(({ owner, repo, path }) => {
+    browser.storage.local.get().then(({ owner, repo, path, token }) => {
       if (!owner || !repo || !path) {
         return
       }
+      const options: any = { owner, repo, path }
+      if (token) {
+        options.auth = token
+      }
       const ocktokit = new Octokit()
-      ocktokit
-        .request('GET /repos/{owner}/{repo}/contents/{path}', { owner, repo, path })
-        .then((res) => {
-          // @ts-expect-error For us, the returned type is not an Array
-          const templates = atob(res.data.content)
-          browser.storage.local.set({ templates })
-        })
+      ocktokit.request('GET /repos/{owner}/{repo}/contents/{path}', options).then((res) => {
+        // @ts-expect-error For us, the returned type is not an Array
+        const templates = atob(res.data.content)
+        browser.storage.local.set({ templates })
+      })
     })
   }
 
